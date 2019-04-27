de2be1175005bd5abc043f353770d88926e95046
Tschesnek <49870356+Tschesnek@users.noreply.github.com>
2019-04-27 12:19:22 +0200

    Revert "as"
    This reverts commit 0660f6629039b0e739899d1c3de4af5f8e3d21d8.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -8,7 +8,6 @@ public class Enemy : MonoBehaviour
     public float speed;
     private Transform target;
     private float origY;
-    private float posX;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +22,7 @@ public class Enemy : MonoBehaviour
     {
         if (Vector2.Distance(transform.position, target.position) < 5)
         {
-            if (transform.position.x == target.position.x)
-            {
-                posX = transform.position.x;
-                transform.position = new Vector2(posX < target.position.x ? ++posX : --posX, transform.position.y);
-                
-            }
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
     }
 }

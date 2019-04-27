de57ef0b882f6398433a90d925447d81fcd25ceb
Tschesnek <49870356+Tschesnek@users.noreply.github.com>
2019-04-27 12:24:15 +0200

    asd

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,13 +7,11 @@ public class Enemy : MonoBehaviour
 
     public float speed;
     private Transform target;
-    private float origY;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        origY = transform.position.y;
 
     }
 

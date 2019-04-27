8becb73bc318070b88719891e4e3c08478a2231c
Tschesnek <49870356+Tschesnek@users.noreply.github.com>
2019-04-27 11:41:59 +0200

    Tschesnek

---
+++ Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed;
+    private Transform target;
+    private float origY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        origY = transform.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) < 4)
+        {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}

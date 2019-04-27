04d070574c7773602c1ed0dfbda368c4cc397a30
Tschesnek <49870356+Tschesnek@users.noreply.github.com>
2019-04-27 18:06:25 +0200

    YEET

--- Assets/Scripts/Items/Equipment.cs
+++ Assets/Scripts/Items/Equipment.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
 public class Equipment : Item
 {
-    
+    public EquipmentSlot equipSlot;
+
+    public int armorModifier;
+    public int damageModifier;
 }
+
+public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield, Feet }

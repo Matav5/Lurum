e230a51b74f86d40aaec5a7b689db20a62cdee51
Tschesnek <49870356+Tschesnek@users.noreply.github.com>
2019-04-27 17:48:51 +0200

    HUGE UPDATE

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed;
+    private Transform target;
+    private bool readyToAttack = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) < 5f && Vector2.Distance(transform.position, target.position) >= 1f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+        if(Vector2.Distance(transform.position, target.position) <= 1f)
+        {
+            if (readyToAttack)
+            {
+                StartCoroutine(damagePlayer());
+            }
+        }
+        
+    }
+
+    private IEnumerator damagePlayer() {
+        readyToAttack = false;
+        //Debug.Log("Player damaged!");
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>().takeDamage(10);
+        yield return new WaitForSeconds(1f);
+        readyToAttack = true;
+    }
+}
---
+++ Assets/Scripts/Items/OnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    public Item item;
+
+    void OnMouseUp()
+    {
+        pickUp();
+    }
+
+    void pickUp()
+    {
+        Debug.Log("Picking up " + item.name);
+        bool wasPickedUp = Inventory.instance.Add(item);
+        if (wasPickedUp)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+}

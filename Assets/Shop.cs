4fec907f9e1b939175a1005a877ca8bb5579cd82
Tschesnek <49870356+Tschesnek@users.noreply.github.com>
2019-04-28 10:15:44 +0200

    ShopStart

---
+++ Assets/Shop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static Shop instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one istance of Shop found!");
+            return;
+        }
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject shopUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleShop()
+    {
+        shopUI.SetActive(!shopUI.activeSelf);
+    }
+
+    public void Buy()
+    {
+
+    }
+}
